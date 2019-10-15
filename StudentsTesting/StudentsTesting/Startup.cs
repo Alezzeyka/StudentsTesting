a0d0bbfe581e204cce13776876e0188e415eafe2
Alezzeyka <49941472+Alezzeyka@users.noreply.github.com>
2019-10-15 14:05:16 +0300

    v001
    start

---
+++ StudentsTesting/StudentsTesting/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(StudentsTesting.Startup))]
+namespace StudentsTesting
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
